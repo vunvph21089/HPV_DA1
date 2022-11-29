e39b8600ba2518750676df25e25fbbd0dd525362
gr10 <hoangpvph21107@fpt.edu.vn>
2022-11-29 17:30:14 +0700

    model/pdo-dbConfig

---
+++ model/pdo.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Mở kết nối đến CSDL sử dụng PDO
+ */
+function pdo_get_connection()
+{
+    $dburl = "mysql:host=localhost;dbname=HPV;charset=utf8";
+    $username = 'root';
+    $password = '';
+
+    $conn = new PDO($dburl, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+}
+// hàm thực thi
+function pdo_execute($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+// hàm thêm
+function pdo_execute_return_lastInsertID($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        return $conn->lastInsertId();
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+
+// lấy danh sách của bảng
+function pdo_query($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $rows = $stmt->fetchAll();
+        return $rows;
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+// lấy 1 phần tử trong danh sách
+function pdo_query_one($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+// giá trị của 1 phần tử trong mảng
+function pdo_query_value($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return array_values($row)[0];
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+function pdo_query_last_id($sql)
+{
+    //Get last inserted id
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "HPV";
+    //Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    //Check connection
+    if ($conn->connect_error) {
+        die("failed: " . $conn->connect_error);
+    }
+
+    //Action
+    if ($conn->query($sql)) {
+        $last_id = $conn->insert_id;
+        return $last_id;
+    } else {
+        echo "error" . $sql . "</br>" . $conn->error;
+    }
+
+    //Close connection
+    $conn->close();
+}
